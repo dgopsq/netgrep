35e722312b8c3f3845753e451ffc250f64622d9a
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-08-12 12:07:50 +0200

    Create an initial `core` implementation

---
+++ packages/core/src/lib.rs
@@ -0,0 +1,35 @@
+use grep::printer::StandardBuilder;
+use grep::regex::RegexMatcher;
+use grep::searcher::{BinaryDetection, SearcherBuilder};
+use wasm_bindgen::prelude::*;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn netgrep(chunk: &[u8], pattern: &str) -> JsValue {
+    let result = search(chunk, pattern);
+
+    match result {
+        true => JsValue::TRUE,
+        false => JsValue::FALSE,
+    }
+}
+
+pub fn search(chunk: &[u8], pattern: &str) -> bool {
+    let matcher = RegexMatcher::new_line_matcher(pattern).unwrap();
+
+    let mut searcher = SearcherBuilder::new()
+        .binary_detection(BinaryDetection::quit(b'\x00'))
+        .line_number(false)
+        .build();
+
+    let mut printer = StandardBuilder::new()
+        .only_matching(true)
+        .build_no_color(Vec::new());
+
+    let _ = searcher.search_slice(&matcher, chunk, printer.sink(&matcher));
+
+    return printer.has_written();
+}
