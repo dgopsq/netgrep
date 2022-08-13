8fa23d0e949a69218192d2bf693d0fcba21454df
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-08-13 15:54:07 +0200

    Update example using debounce

--- packages/example/src/App.jsx
+++ packages/example/src/App.jsx
@@ -1,40 +1,32 @@
-import { useRef, useState, useCallback, useMemo } from 'react';
+import { useState, useCallback } from 'react';
+import debounce from 'lodash/debounce';
 import { Netgrep } from 'netgrep';
 
 const NG = new Netgrep({});
 
 export function App() {
-  const [searchId, setSearchId] = useState(0);
-  const resultsRef = useRef({});
+  const [results, setResults] = useState([]);
 
   const handleChange = useCallback(
-    (e) => {
+    debounce((e) => {
       const pattern = e.target.value || '';
 
-      const nextSearchId = searchId + 1;
-      resultsRef.current[nextSearchId] = [];
-      setSearchId(nextSearchId);
+      if (pattern === '') {
+        setResults([]);
+        return;
+      }
 
       NG.searchBatch(
         [
           'https://www.dgopsq.space/_next/data/ccLss2WP7L9CS5HZBe8uh/blog/inflist-an-experiment-using-purescript-and-react.json?slug=inflist-an-experiment-using-purescript-and-react',
           'https://www.dgopsq.space/_next/data/ccLss2WP7L9CS5HZBe8uh/blog/reading-from-stdin-using-purescript.json?slug=reading-from-stdin-using-purescript',
-          'https://www.dgopsq.space/_next/data/ccLss2WP7L9CS5HZBe8uh/blog/reading-from-stdin-using-purescript.json?slug=reading-from-stdin-using-purescript',
           'https://www.dgopsq.space/_next/data/ccLss2WP7L9CS5HZBe8uh/blog/auth0-expo-integration.json?slug=auth0-expo-integration',
           'https://www.dgopsq.space/_next/data/ccLss2WP7L9CS5HZBe8uh/blog/handling-migrations-rn-sqlite-fp-ts.json?slug=handling-migrations-rn-sqlite-fp-ts',
         ],
-        pattern,
-        (result) => {
-          resultsRef.current[nextSearchId].push(result);
-        }
-      );
-    },
-    [searchId, setSearchId, resultsRef]
-  );
-
-  const results = useMemo(
-    () => resultsRef.current[searchId] || [],
-    [resultsRef, searchId]
+        pattern
+      ).then(setResults);
+    }, 500),
+    [setResults]
   );
 
   return (
@@ -42,9 +34,9 @@ export function App() {
       <input type='text' onChange={handleChange} />
 
       <ul>
-        {results.map((result) => (
-          <li key={result.url}>{result.url}</li>
-        ))}
+        {results.map(({ result, url }) =>
+          result ? <li key={url}>{url}</li> : null
+        )}
       </ul>
     </div>
   );
